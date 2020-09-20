b27dd84e1bfbd972eafb2c9d1121f0ab3e3ba315
Alexander Flensborg <alexanderflensborg@hotmail.dk>
2020-09-20 15:25:23 +0200

    Updated gitignore, and added training script

---
+++ train_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for i in {1..20}; do
+	python3 cli.py --train --save_dir '.save' --num_epochs 100 --batch_size 32 --lr 0.01 --task $i --memory_size 50 --num_hops 3 --max_clip 40.0 --tenk >> logs/task$i.txt;
+done
